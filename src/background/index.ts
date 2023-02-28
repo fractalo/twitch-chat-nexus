f7f08b7e5d0e7c124c9740d4411740ea352630c6
fractalo <42487844+fractalo@users.noreply.github.com>
2023-02-28 18:37:08 +0900

    initial setup
    based on https://github.com/NekitCorp/chrome-extension-svelte-typescript-boilerplate

---
+++ src/background/index.ts
@@ -0,0 +1,6 @@
+
+chrome.runtime.onInstalled.addListener(() => {
+    
+});
+
+export {}
---
+++ src/popup/index.ts
@@ -0,0 +1,9 @@
+import "../app.css";
+
+const target = document.getElementById('app');
+
+async function render() {
+    
+}
+
+document.addEventListener('DOMContentLoaded', render);
