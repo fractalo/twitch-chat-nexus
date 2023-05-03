be20ecdf52fc25d4607204851123a1aff7cd6fd8
fractalo <42487844+fractalo@users.noreply.github.com>
2023-05-04 01:26:11 +0900

    feat: Emit event on twitch route change

---
+++ src/injected/routes/index.ts
@@ -0,0 +1,46 @@
+import historyObserver from '../observers/historyObserver';
+import routeMatchers from './routeMatchers';
+import { type Emitter, createNanoEvents } from "nanoevents";
+
+
+interface Events {
+    change: (routeName: string) => void;
+}
+
+class Routes {
+    private emitter: Emitter;
+    private routeName: string;
+
+    constructor() {
+        this.emitter = createNanoEvents<Events>();
+        this.routeName = this.getCurrentRouteName();
+
+        historyObserver.on('locationChange', () => {
+            this.routeName = this.getCurrentRouteName(); 
+            this.emitter.emit('change', this.routeName);
+        });
+    }
+
+    on<E extends keyof Events>(event: E, callback: Events[E]) {
+        return this.emitter.on(event, callback);
+    }
+
+    private getCurrentRouteName() {
+        const path = location.pathname;
+        const state = history.state;
+
+        const route = routeMatchers.find((matcher) => {
+            const match = matcher.regex.exec(path);
+            return match && (!matcher.matchState || matcher.matchState(state));
+        });
+
+        return route?.name || '';
+    }
+
+    get name() {
+        return this.routeName;
+    }
+}
+
+
+export default new Routes();
---
+++ src/injected/routes/routeMatchers.ts
@@ -0,0 +1,58 @@
+import { pathToRegexp } from 'path-to-regexp';
+
+interface RouteMatcher {
+    name: string;
+    regex: RegExp;
+    matchState?: (state: any) => boolean;
+}
+
+// https://github.com/FrankerFaceZ/FrankerFaceZ/blob/master/src/sites/twitch-twilight/index.js
+const TWITCH_ROUTES = {
+    'front-page': '/',
+	'collection': '/collections/:collectionID',
+	'dir': '/directory',
+	'dir-following': '/directory/following/:category?',
+	'dir-game-index': '/directory/game/:gameName',
+	'dir-game-clips': '/directory/game/:gameName/clips',
+	'dir-game-videos': '/directory/game/:gameName/videos/:filter',
+	'dir-all': '/directory/all/:filter?',
+	'event': '/event/:eventName',
+	'chat': '/popout/:loginName/chat',
+	'video': '/videos/:videoID',
+	'channel-video': '/:loginName/video/:videoID',
+	'channel-videos': '/:loginName/videos/:filter?',
+	'channel-clips': '/:loginName/clips',
+	'channel-clip': '/:loginName/clip/:clipID',
+	'channel-collections': '/:loginName/collections',
+	'channel-events': '/:loginName/events',
+	'product': '/products/:productName',
+	'turbo': '/turbo',
+	'search': '/search',
+	'squad': '/:loginName/squad',
+	'command-center': '/:loginName/commandcenter',
+	'embed-chat': '/embed/:loginName/chat',
+	'mod-view': '/moderator/:loginName',
+	'mod-popout-chat': '/popout/moderator/:loginName/chat'
+};
+
+const routeMatchers: RouteMatcher[] = Object.entries(TWITCH_ROUTES).map(([name, route]) => {
+    return {
+        name,
+        regex: pathToRegexp(route)
+    };
+});
+
+routeMatchers.push(
+    {
+        name: 'channel',
+        regex: pathToRegexp('/:loginName'),
+        matchState: state => state?.state?.channelView !== 'Home'
+    }, 
+    {
+        name: 'channel-home',
+        regex: pathToRegexp('/:loginName'),
+        matchState: state => state?.state?.channelView === 'Home'
+    }
+);
+
+export default routeMatchers;
