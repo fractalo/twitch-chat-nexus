cac4e39ef25b4b3c139cb4f477131e6f84ea9e62
fractalo <42487844+fractalo@users.noreply.github.com>
2023-05-04 00:54:27 +0900

    design(main): Modify daisyui color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,43 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: [
+      {
+        light: {
+          "color-scheme": "light",
+          primary: "#9147ff",
+          "primary-content": "#ffffff",
+          secondary: "#f000b8",
+          "secondary-content": "#ffffff",
+          accent: "#37cdbe",
+          "accent-content": "#163835",
+          neutral: "#3d4451",
+          "neutral-content": "#ffffff",
+          "base-100": "#ffffff",
+          "base-200": "#F2F2F2",
+          "base-300": "#E5E6E6",
+          "base-content": "#1f2937",
+        }
+      }, 
+      {
+        dark: {
+          "color-scheme": "dark",
+          primary: "#7c3de3",
+          "primary-content": "#ffffff",
+          secondary: "#D926AA",
+          "secondary-content": "#ffffff",
+          accent: "#1FB2A5",
+          "accent-content": "#ffffff",
+          neutral: "#27272d",
+          "neutral-focus": "#111318",
+          "neutral-content": "#ffffff",
+          "base-100": "#18181b",
+          "base-200": "#1f1f23",
+          "base-300": "#26262c",
+          "base-content": "#efeff1",
+        }
+      }
+    ],
     darkTheme: null,
   },
 }
