8959a80afe8c7bf0a2c35e824d7573e56322ff93
fractalo <42487844+fractalo@users.noreply.github.com>
2023-05-04 02:24:04 +0900

    feat: Add responseCallback to Messaging component

--- src/components/Messaging.ts
+++ src/components/Messaging.ts
@@ -11,20 +11,30 @@ interface Message {
     content?: any;
     from: string;
     to?: string;
+    contextId?: string;
 }
+
+interface PostMessageConfig {
+    type: string;
+    content?: any;
+    to?: string;
+    contextId?: string;
+}
+
+type ResponseCallback = (response: Message) => void;
 
 export class Messaging {
     private id: string;
     private emitter: Emitter;
     private connections: Set<string>;
-    private allowedIds: Set<string>;
+    private responseCallbacks: Map<string, ResponseCallback>;
     
 
-    constructor(id: string, allowedIds: string[] = []) {
-        this.id = id;
+    constructor(selfId: string) {
+        this.id = selfId;
         this.emitter = createNanoEvents<Events>();
         this.connections = new Set();
-        this.allowedIds = new Set(allowedIds);
+        this.responseCallbacks = new Map();
 
         window.addEventListener('message', (event) => {
             const message = event.data as Message;
@@ -32,7 +42,6 @@ export class Messaging {
                 event.source !== window || 
                 !message.from || 
                 message.from === this.id || 
-                (this.allowedIds.size && !this.allowedIds.has(message.from)) ||
                 (message.to && message.to !== this.id)
             ) {
                 return;
@@ -41,32 +50,65 @@ export class Messaging {
             if (message.type === 'READY') {
                 if (this.connections.has(message.from)) return;
                 
-                this.postMessage('READY');
+                this.postMessage({type: "READY"});
                 this.connections.add(message.from);
                 this.emitter.emit('connect', message.from);
                 return;
             }
 
             this.emitter.emit('message', message);
+
+            if (message.contextId) {
+                const callback = this.responseCallbacks.get(message.contextId);
+                this.responseCallbacks.delete(message.contextId);
+                callback && callback(message);
+            }
         });
-        this.postMessage('READY');
+        this.postMessage({type: "READY"});
     }
 
     on<E extends keyof Events>(event: E, callback: Events[E]) {
         return this.emitter.on(event, callback);
     }
 
-    postMessage(type: string, content?: any, to?: string) {
+    postMessage(config: PostMessageConfig, responseCallback?: ResponseCallback) {
+        const contextId = config.contextId || crypto?.randomUUID();
+
+        responseCallback && this.responseCallbacks.set(contextId, responseCallback);  
+
         const message: Message = {
-            type,
-            content,
+            type: config.type,
+            content: config.content,
             from: this.id,
-            to,
+            to: config.to,
+            contextId
         };
+
         try {
             window.postMessage(message, '*');
         } catch (error) {
             console.error('postMessage error!', error);
+            this.responseCallbacks.delete(contextId);
         }
     }
+
+    isConnected(id: string) {
+        return this.connections.has(id);
+    }
+
+    async waitForConnected(targetId: string) {
+        if (this.isConnected(targetId)) {
+            return;
+        }
+
+        return new Promise<void>((resolve) => {
+            const unbind = this.on('connect', (id) => {
+                if (id === targetId) {
+                    unbind();
+                    resolve();
+                }
+            });
+        });
+    }
 }
+
