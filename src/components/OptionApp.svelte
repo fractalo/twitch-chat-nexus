1bd5411d642b926a581b3b93615522201f9614dd
fractalo <42487844+fractalo@users.noreply.github.com>
2023-05-04 02:11:02 +0900

    feat: Add extension options page

---
+++ src/components/OptionApp.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import Settings from "./settings/Settings.svelte";
+</script>
+
+<div class="h-[24rem]">
+    <Settings />
+</div>
--- src/options/index.ts
+++ src/options/index.ts
@@ -1,1 +1,15 @@
 import "../app.css";
+import 'simplebar';
+// import 'simplebar/dist/simplebar.css';
+import OptionApp from "src/components/OptionApp.svelte";
+
+const target = document.getElementById('app');
+
+async function render() {
+    if (!target) return;
+    
+    new OptionApp({ target });
+}
+
+
+document.addEventListener('DOMContentLoaded', render);
