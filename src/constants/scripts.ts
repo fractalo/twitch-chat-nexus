2d267fd9a3b120ce98d6a1d9a5e7afd2514ebdc4
fractalo <42487844+fractalo@users.noreply.github.com>
2023-05-04 03:09:16 +0900

    refactor: Update script ID naming

--- src/constants/scripts.ts
+++ src/constants/scripts.ts
@@ -1,6 +1,6 @@
-const PREFIX = "MTC_";
+export const SCRIPT_ID_PREFIX = "TCH";
 
-export const scriptIds = {
-    contentScript: `${PREFIX}CONTENT_SCRIPT`,
-    injectedScript: `${PREFIX}INJECTED_SCRIPT`,
+export const ScriptIds = {
+    content: `${SCRIPT_ID_PREFIX}_CONTENT_SCRIPT`,
+    injected: `${SCRIPT_ID_PREFIX}_INJECTED_SCRIPT`,
 } as const;
