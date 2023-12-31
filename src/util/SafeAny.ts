7879ccded8bf293309dbff8198992d29efa83a6a
fractalo <42487844+fractalo@users.noreply.github.com>
2024-01-01 03:55:40 +0900

    refactor: remove unused exported function
    - moved to type predicates util group

--- src/util/SafeAny.ts
+++ src/util/SafeAny.ts
@@ -1,7 +1,3 @@
 export type SafeAny = {
     [key: PropertyKey]: SafeAny,
 } | undefined | null;
-
-export const isRecord = (value: unknown): value is Record<string, unknown> => {
-    return typeof value === 'object' && value !== null;
-}
