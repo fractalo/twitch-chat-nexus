eb4929126a1a050c2114c50536a8d4edf3f92148
fractalo <42487844+fractalo@users.noreply.github.com>
2024-01-01 02:23:09 +0900

    feat: add TooltipButton component

---
+++ src/components/tooltip/TooltipButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import type { Placement } from "@floating-ui/core";
+  import Tooltip from "./Tooltip.svelte";
+
+  export let placement: Placement;
+  export let label: string;
+  export let disabled: boolean = false;
+
+  let showTooltip = false;
+
+</script>
+
+<Tooltip 
+  {placement}
+  {showTooltip}
+  text={label}
+  let:tooltipRef
+>
+  <button 
+    class={$$props.class}
+    on:mouseenter={() => showTooltip = true}
+    on:mouseleave={() => showTooltip = false}
+    on:click
+    {disabled}
+    use:tooltipRef
+  >
+    <slot />
+  </button>
+</Tooltip>
