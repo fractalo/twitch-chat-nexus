4760164f48e0639b99aaa25925d2f5fcf0e33c57
fractalo <42487844+fractalo@users.noreply.github.com>
2024-01-01 19:05:12 +0900

    feat(ChatClient): emit event on join

--- src/injected/interceptor/clients/ChatClient/index.ts
+++ src/injected/interceptor/clients/ChatClient/index.ts
@@ -1,22 +1,27 @@
-import { createNanoEvents } from "nanoevents";
-import type { Message } from "ircv3/lib";
+import { createNanoEvents, type Unsubscribe } from "nanoevents";
 import { parseMessage } from "./parseMessage";
 import { isChatWebSocketUrl } from "./isChatWebSocketUrl";
+import { MessageTypes, type Message, type MessageConstructor } from "ircv3"; 
+import type { EventHandler } from "node_modules/ircv3/lib/IrcClient";
+import { toUserName } from "@twurple/chat";
 
 
-interface Events {
+export interface Events {
     send: (message: Message<any>) => void;
     receive: (message: Message<any>) => void;
     create: () => void;
     ping: () => void;
     open: () => void;
+    join: (channel: string) => void;
 }
 
 export interface ChatClient {
-    on<E extends keyof Events>(event: E, callback: Events[E]): void;
+    on<E extends keyof Events>(event: E, callback: Events[E]): Unsubscribe;
     isOpened(): boolean;
     sendPing(sendWithUUID?: boolean): void;
 }
+
+type EventHandlers<T extends Message> = EventHandler<T>[];
 
 const createChatClient = (): ChatClient => {
     const emitter = createNanoEvents<Events>();
@@ -71,10 +76,8 @@ const createChatClient = (): ChatClient => {
         webSocket.addEventListener('open', openListener);
         webSocket.addEventListener('close', closeListener);
 
-
         ircWebSocket = webSocket;
     };
-
 
     window.WebSocket = class extends WebSocket {
         constructor(url: string | URL, protocols?: string | string[]) {
@@ -96,11 +99,33 @@ const createChatClient = (): ChatClient => {
         if (!isOpened()) return;
 
         if (sendWithUUID) {
-            ircWebSocket?.send(`PING :${crypto?.randomUUID()}`);
+            ircWebSocket?.send(`PING :${window.crypto?.randomUUID()}`);
         } else {
             ircWebSocket?.send('PING');
         }
     };
+
+    const events = new Map<string, EventHandlers<any>>();
+
+    const onTypedMessage = <T extends Message>(
+        type: MessageConstructor<T>,
+        handler: EventHandler<T>
+    ) => {
+        const handlers: EventHandlers<any> = events.get(type.COMMAND) || [];
+        handlers.push(handler);
+        events.set(type.COMMAND, handlers);
+    };
+
+    onTypedMessage(MessageTypes.Commands.ChannelJoin, ({ channel }) => {
+        emitter.emit('join', toUserName(channel));
+    });
+
+    emitter.on('receive', (message) => {
+        const handlers = events.get(
+            (message.constructor as MessageConstructor).COMMAND
+        );
+        handlers?.forEach(handler => handler(message));
+    });
 
     return { on, isOpened, sendPing };
 };
