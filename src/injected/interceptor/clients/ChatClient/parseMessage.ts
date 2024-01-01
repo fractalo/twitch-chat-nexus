40f436bb9fff1b2c51f97146a51e85f3d07bddd9
fractalo <42487844+fractalo@users.noreply.github.com>
2024-01-01 19:08:18 +0900

    refactor: specify return type for parseMessage

--- src/injected/interceptor/clients/ChatClient/parseMessage.ts
+++ src/injected/interceptor/clients/ChatClient/parseMessage.ts
@@ -1,7 +1,7 @@
 import { parseTwitchMessage } from "@twurple/chat";
+import type { Message } from "node_modules/ircv3/lib";
 
-
-export const parseMessage = (line: string) => {
+export const parseMessage = (line: string): Message | null => {
     try {
         return parseTwitchMessage(line);
     } catch(error) {
